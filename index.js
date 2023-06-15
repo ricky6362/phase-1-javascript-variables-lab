b0fff4241b62505f841e71f794103158a72fe821
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-15 16:21:46 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Declaring a variable using const
+const companyName = "Scuber";
+
+// Declaring a variable using let
+let mostProfitableNeighborhood = "Chelsea"
+
+// Another variablue using let
+let companyCeo = "Susan Smith"
